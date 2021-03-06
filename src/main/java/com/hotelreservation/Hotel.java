d0854b0c0be68eb1a6db136b1b1fa43a73521b6b
shravan-kaja <shravan.kaja@gmail.com>
2021-03-06 10:19:56 +0530

    Added: gradle file and initialized classes

---
+++ src/main/java/com/hotelreservation/Hotel.java
@@ -0,0 +1,4 @@
+package com.hotelreservation;
+
+public class Hotel {
+}
---
+++ src/main/java/com/hotelreservation/ReservationSystem.java
@@ -0,0 +1,8 @@
+package com.hotelreservation;
+
+public class ReservationSystem {
+
+    public static void main(String Args[]) {
+        System.out.println("Welcome to hotel Reservation System");
+    }
+}
