c0fa00c69e07b5ca99373e6a6f9d5dc1941a2b85
shravan-kaja <shravan.kaja@gmail.com>
2021-03-06 10:33:12 +0530

    Added: initialied hotel class and variables aacosiated

--- src/main/java/com/hotelreservation/Hotel.java
+++ src/main/java/com/hotelreservation/Hotel.java
@@ -1,4 +1,31 @@
 package com.hotelreservation;
 
 public class Hotel {
+    public String hotelName;
+    public int rating;
+    public int weekDayRates;
+    public int weekEndRates;
+
+    public Hotel(String hotelName, int rating, int weekDayRates, int weekEndRates) {
+        this.hotelName = hotelName;
+        this.rating = rating;
+        this.weekDayRates = weekDayRates;
+        this.weekEndRates = weekEndRates;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getWeekDayRates() {
+        return weekDayRates;
+    }
+
+    public int getWeekEndRates() {
+        return weekEndRates;
+    }
 }
--- src/main/java/com/hotelreservation/ReservationSystem.java
+++ src/main/java/com/hotelreservation/ReservationSystem.java
@@ -4,5 +4,6 @@ public class ReservationSystem {
 
     public static void main(String Args[]) {
         System.out.println("Welcome to hotel Reservation System");
+        Hotel lakeWood=new Hotel("LakeWood",5,110,120);
     }
 }
