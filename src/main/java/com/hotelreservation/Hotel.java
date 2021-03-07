c89a741e7fffaa2179eba590ba0380f154d537eb
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 08:06:54 +0530

    Added: weekday and WeekEnd rates for eaach hotel

--- src/main/java/com/hotelreservation/Hotel.java
+++ src/main/java/com/hotelreservation/Hotel.java
@@ -1,20 +1,20 @@
 package com.hotelreservation;
-import java.util.*;
-import java.util.stream.*;
 
 import java.util.*;
 import java.util.stream.*;
 
 public class Hotel {
     public String name;
-    public int rate;
-    ArrayList<Hotel> arrayofHotels = new ArrayList<>();
+    public int weekDayRate;
+    public int weekEndRate;
+    ArrayList<Hotel> arrayOfHotels = new ArrayList<>();
     Map<String, Integer> costOfHotels = new HashMap<>();
     Scanner sc = new Scanner(System.in);
 
-    public Hotel(String name, int rate) {
+    public Hotel(String name, int weekDayRate, int weekEndRate) {
         this.name = name;
-        this.rate = rate;
+        this.weekDayRate = weekDayRate;
+        this.weekEndRate = weekEndRate;
     }
 
     public Hotel() {
@@ -23,32 +23,33 @@ public class Hotel {
 
     // method to add hotel
     public boolean addHotel(Hotel hotel) {
-        arrayofHotels.add(hotel);
+        arrayOfHotels.add(hotel);
         return true;
     }
 
+    // method to find cheap hotel for given date range
     public int findCheapestHotel(String dateCheckIn, String dateCheckOut) {
         String dayStart = dateCheckIn.substring(0, 2);
         int checkInDay = Integer.parseInt(dayStart);
         String dayEnd = dateCheckOut.substring(0, 2);
         int checkOutDay = Integer.parseInt(dayEnd);
         int numberOfDays = checkOutDay - checkInDay;
-        costOfHotels = arrayofHotels.stream().collect(Collectors.toMap(e -> e.getHotelName(), e -> e.getRate() * numberOfDays));
-        String hotel = Collections.min(costOfHotels.keySet());
-        int cost = costOfHotels.get(hotel);
-        System.out.println("Minimm Hotel is " + hotel + " :" + cost);
+        costOfHotels = arrayOfHotels.stream().collect(Collectors.toMap(e -> e.getHotelName(), e -> e.getWeekDayRate() * numberOfDays));
+        int cost = Collections.min(costOfHotels.values());
+        costOfHotels.entrySet().stream().filter(e -> e.getValue().equals(cost)).forEach(e -> System.out.println(e.getKey()));
+        System.out.println("Stay Price :" + cost);
         return cost;
-    }
-
-    public void calculatePrice() {
-
     }
 
     public String getHotelName() {
         return name;
     }
 
-    public int getRate() {
-        return rate;
+    public int getWeekDayRate() {
+        return weekDayRate;
+    }
+
+    public int getWeekEndRate() {
+        return weekEndRate;
     }
 }
