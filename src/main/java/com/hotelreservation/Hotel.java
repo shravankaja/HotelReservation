945b331be5fbacb52962d5644aaab2003eb743c9
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 07:17:30 +0530

    Added: find cheapest method using streams

--- src/main/java/com/hotelreservation/Hotel.java
+++ src/main/java/com/hotelreservation/Hotel.java
@@ -1,17 +1,18 @@
 package com.hotelreservation;
 
 import java.util.*;
+import java.util.stream.*;
 
 public class Hotel {
     public String name;
     public int rate;
-    ArrayList<Hotel> arrayofHotel = new ArrayList<>();
+    ArrayList<Hotel> arrayofHotels = new ArrayList<>();
+    Map<String, Integer> costOfHotels = new HashMap<>();
     Scanner sc = new Scanner(System.in);
 
     public Hotel(String name, int rate) {
         this.name = name;
         this.rate = rate;
-
     }
 
     public Hotel() {
@@ -20,15 +21,32 @@ public class Hotel {
 
     // method to add hotel
     public boolean addHotel(Hotel hotel) {
-        arrayofHotel.add(hotel);
+        arrayofHotels.add(hotel);
         return true;
+    }
+
+    public int findCheapestHotel(String dateCheckIn, String dateCheckOut) {
+        String dayStart = dateCheckIn.substring(0, 2);
+        int checkInDay = Integer.parseInt(dayStart);
+        String dayEnd = dateCheckOut.substring(0, 2);
+        int checkOutDay = Integer.parseInt(dayEnd);
+        int numberOfDays = checkOutDay - checkInDay;
+        costOfHotels = arrayofHotels.stream().collect(Collectors.toMap(e -> e.getHotelName(), e -> e.getRate() * numberOfDays));
+        String hotel = Collections.min(costOfHotels.keySet());
+        int cost = costOfHotels.get(hotel);
+        System.out.println("Minimm Hotel is " + hotel + " :" + cost);
+        return cost;
+    }
+
+    public void calculatePrice() {
+
     }
 
     public String getHotelName() {
         return name;
     }
 
-    public int getRating() {
+    public int getRate() {
         return rate;
     }
 }
