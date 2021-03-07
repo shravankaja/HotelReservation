627fb054fb1b518104509a0b53a9c40f070d6fe7
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 06:14:13 +0530

    Refactored: removed unwanted hotel variables and used hotel add ethod to add a hotel

--- src/main/java/com/hotelreservation/ReservationSystem.java
+++ src/main/java/com/hotelreservation/ReservationSystem.java
@@ -3,31 +3,16 @@ package com.hotelreservation;
 import java.util.*;
 
 public class ReservationSystem {
-    public String hotelName;
-    static String name;
-    static int rating;
-    static int weekDayRates;
-    static int weekEndRates;
-
-    // method to add hotel
-    public boolean addHotel(String name, int rating, int weekDayRates, int weekEndRates) {
-        hotelName = name;
-        Hotel hotelName = new Hotel(name, rating, weekDayRates, weekEndRates);
-        return true;
-    }
 
     public static void main(String Args[]) {
+        Hotel hotel = new Hotel();
         System.out.println("Welcome to hotel Reservation System");
         ReservationSystem reservationSystem = new ReservationSystem();
-        System.out.println("Enter Hotel Name");
-        Scanner sc = new Scanner(System.in);
-        name = sc.nextLine();
-        System.out.println("Enter Ratings");
-        rating = sc.nextInt();
-        System.out.println("Enter Week Day rates");
-        weekDayRates = sc.nextInt();
-        System.out.println("Enter WeekEnd Rates");
-        weekEndRates = sc.nextInt();
-        reservationSystem.addHotel(name, rating, weekDayRates, weekDayRates);
+        Hotel lakeWood = new Hotel("LakeWood", 110);
+        Hotel bridgeWood = new Hotel("BridgeWood", 130);
+        Hotel ridgeWood = new Hotel("RidgeWood", 140);
+        hotel.addHotel(lakeWood);
+        hotel.addHotel(bridgeWood);
+        hotel.addHotel(ridgeWood);
     }
 }
