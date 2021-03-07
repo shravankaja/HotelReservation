691e66c186053778b0f3b3d3b08c9c28474a0189
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 10:34:22 +0530

    Added: test case to check if the new cost is returned affter considering weekends also

--- src/test/java/com/hotelreservation/HotelTest.java
+++ src/test/java/com/hotelreservation/HotelTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.*;
 public class HotelTest {
     ReservationSystem reservationSystem = new ReservationSystem();
     Hotel hotel = new Hotel();
+    Hotel lakeWood = new Hotel("LakeWood", 110, 90);
+    Hotel bridgeWood = new Hotel("BridgeWood", 150, 50);
+    Hotel ridgeWood = new Hotel("RidgeWood", 220, 150);
 
     @Test
     void givenHotelNameAndRateCheckIfHotelIsAddedToSystem() {
@@ -16,13 +19,18 @@ public class HotelTest {
 
     @Test
     void givenDateRangeWeShouldGetHotelWithMinimumPrice() {
-        Hotel lakeWood = new Hotel("LakeWood", 110, 90);
-        Hotel bridgeWood = new Hotel("BridgeWood", 150, 50);
-        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150);
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
         Assertions.assertEquals(330, hotel.findCheapestHotel("12Sep2020", "15Sep2020"));
+    }
+
+    @Test
+    void givenDateRangeWeShouldGetPriceAccordingToWeekDayAndWeekEnds() {
+        hotel.addHotel(lakeWood);
+        hotel.addHotel(bridgeWood);
+        hotel.addHotel(ridgeWood);
+        Assertions.assertEquals(1530, hotel.findCheapestHotel("12Sep2020", "27Sep2020"));
     }
 }
 
