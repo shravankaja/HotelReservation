1b1faa9dd67c2b7aba5a8691166f1a84b6c2ea8a
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 19:56:12 +0530

    Added: hotels with reward customers rates

--- src/main/java/com/hotelreservation/ReservationSystem.java
+++ src/main/java/com/hotelreservation/ReservationSystem.java
@@ -8,9 +8,9 @@ public class ReservationSystem {
         Hotel hotel = new Hotel();
         System.out.println("Welcome to hotel Reservation System");
         ReservationSystem reservationSystem = new ReservationSystem();
-        Hotel lakeWood = new Hotel("LakeWood", 110, 90, 3);
-        Hotel bridgeWood = new Hotel("BridgeWood", 150, 50, 4);
-        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150, 5);
+        Hotel lakeWood = new Hotel("LakeWood", 110, 90, 3, 80, 80);
+        Hotel bridgeWood = new Hotel("BridgeWood", 150, 60, 4, 110, 50);
+        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150, 5, 100, 40);
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
