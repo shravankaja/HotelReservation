d62ce7c832509cf512a4cf24fcb221a872e6a626
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 07:29:47 +0530

    Added: test case to check if minimum hotel price is returned

--- src/test/java/com/hotelreservation/HotelTest.java
+++ src/test/java/com/hotelreservation/HotelTest.java
@@ -3,10 +3,24 @@ package com.hotelreservation;
 import org.junit.jupiter.api.*;
 
 public class HotelTest {
+    ReservationSystem reservationSystem = new ReservationSystem();
+    Hotel hotel = new Hotel();
+
     @Test
-    void checkHotelAdded() {
-        ReservationSystem reservationSystem = new ReservationSystem();
-        boolean result = reservationSystem.addHotel("LakeWood",4,110,120);
-        Assertions.assertTrue(result);
+    void givenHotelNameAndRateCheckIfHotelIsAddedToSystem() {
+        Hotel lakeWood = new Hotel("lkeWood", 110);
+        hotel.addHotel(lakeWood);
+        Assertions.assertTrue(hotel.arrayofHotels.contains(lakeWood));
+    }
+
+    @Test
+    void givenDateRangeWeShouldGetHotelWithMinimumPrice() {
+        Hotel lakeWood = new Hotel("LakeWood", 160);
+        Hotel bridgeWood = new Hotel("BridgeWood", 170);
+        Hotel ridgeWood = new Hotel("RidgeWood", 140);
+        hotel.addHotel(lakeWood);
+        hotel.addHotel(bridgeWood);
+        hotel.addHotel(ridgeWood);
+        Assertions.assertEquals(510,hotel.findCheapestHotel("12Sep2020","15Sep2020"));
     }
 }
