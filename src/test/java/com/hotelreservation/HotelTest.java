37bfa460471e68bd33d6753df127e2323672e612
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 08:08:51 +0530

    Refactored: test case to accept new weekday and weekend rates

--- src/test/java/com/hotelreservation/HotelTest.java
+++ src/test/java/com/hotelreservation/HotelTest.java
@@ -2,29 +2,27 @@ package com.hotelreservation;
 
 import org.junit.jupiter.api.*;
 
-import java.rmi.server.*;
-
 public class HotelTest {
     ReservationSystem reservationSystem = new ReservationSystem();
     Hotel hotel = new Hotel();
 
     @Test
     void givenHotelNameAndRateCheckIfHotelIsAddedToSystem() {
-        Hotel lakeWood = new Hotel("lkeWood", 110);
+        Hotel lakeWood = new Hotel("LakeWood", 110, 90);
         hotel.addHotel(lakeWood);
-        Assertions.assertTrue(hotel.arrayofHotels.contains(lakeWood));
+        Assertions.assertTrue(hotel.arrayOfHotels.contains(lakeWood));
     }
 
 
     @Test
     void givenDateRangeWeShouldGetHotelWithMinimumPrice() {
-        Hotel lakeWood = new Hotel("LakeWood", 160);
-        Hotel bridgeWood = new Hotel("BridgeWood", 170);
-        Hotel ridgeWood = new Hotel("RidgeWood", 140);
+        Hotel lakeWood = new Hotel("LakeWood", 110, 90);
+        Hotel bridgeWood = new Hotel("BridgeWood", 150, 50);
+        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150);
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
-        Assertions.assertEquals(510, hotel.findCheapestHotel("12Sep2020", "15Sep2020"));
+        Assertions.assertEquals(330, hotel.findCheapestHotel("12Sep2020", "15Sep2020"));
     }
 }
 
