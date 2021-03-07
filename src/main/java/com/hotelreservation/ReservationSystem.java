4703c17e34bb582f5fddb4ddd784d24576cdd787
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 08:07:36 +0530

    Addedd: parameters to new modified constructor

--- src/main/java/com/hotelreservation/ReservationSystem.java
+++ src/main/java/com/hotelreservation/ReservationSystem.java
@@ -8,11 +8,12 @@ public class ReservationSystem {
         Hotel hotel = new Hotel();
         System.out.println("Welcome to hotel Reservation System");
         ReservationSystem reservationSystem = new ReservationSystem();
-        Hotel lakeWood = new Hotel("LakeWood", 110);
-        Hotel bridgeWood = new Hotel("BridgeWood", 130);
-        Hotel ridgeWood = new Hotel("RidgeWood", 140);
+        Hotel lakeWood = new Hotel("LakeWood", 110, 90);
+        Hotel bridgeWood = new Hotel("BridgeWood", 150, 50);
+        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150);
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
+        hotel.findCheapestHotel("12Sep2020","15Sep2020");
     }
 }
