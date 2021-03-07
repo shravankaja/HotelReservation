8f81c1d0c0da9f6e210f1def2d56e7ed3ef35c56
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 19:22:28 +0530

    Added: test case to check if rating and price is accurate for the cheapest hotel

--- src/test/java/com/hotelreservation/HotelTest.java
+++ src/test/java/com/hotelreservation/HotelTest.java
@@ -21,7 +21,7 @@ public class HotelTest {
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
-        Assertions.assertEquals(330, hotel.findCheapestHotel("12Sep2020", "15Sep2020"));
+        Assertions.assertEquals(250, hotel.findCheapestHotel("12Sep2020", "15Sep2020"));
     }
 
     @Test
@@ -29,7 +29,7 @@ public class HotelTest {
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
-        Assertions.assertEquals(200, hotel.findCheapestHotel("11Sep2020", "12Sep2020"));
+        Assertions.assertEquals(50, hotel.findCheapestHotel("11Sep2020", "12Sep2020"));
     }
 
     @Test
@@ -37,6 +37,15 @@ public class HotelTest {
         hotel.addHotel(lakeWood);
         int rating = lakeWood.getRating();
         Assertions.assertEquals(3, rating);
+    }
+
+    @Test
+    void givenDateRangeWeShoudlObtainCheapestHotelWithRating() {
+        hotel.addHotel(lakeWood);
+        hotel.addHotel(bridgeWood);
+        hotel.addHotel(ridgeWood);
+        Assertions.assertEquals(50, hotel.findCheapestHotel("11Sep2020", "12Sep2020"));
+        Assertions.assertEquals(4, hotel.ratingOfHotels.get("BridgeWood"));
     }
 }
 
