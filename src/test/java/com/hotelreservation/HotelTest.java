1037b01ff192a2ccb5edc0e831431889a675e9fe
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 07:19:02 +0530

    Added test caase to verify minimm proce hotel is returned

--- src/test/java/com/hotelreservation/HotelTest.java
+++ src/test/java/com/hotelreservation/HotelTest.java
@@ -12,7 +12,18 @@ public class HotelTest {
     void givenHotelNameAndRateCheckIfHotelIsAddedToSystem() {
         Hotel lakeWood = new Hotel("lkeWood", 110);
         hotel.addHotel(lakeWood);
-        Assertions.assertTrue(hotel.arrayofHotel.contains(lakeWood));
+        Assertions.assertTrue(hotel.arrayofHotels.contains(lakeWood));
+    }
+
+    @Test
+    void givenDateRangeWeShouldGetHotelWithMinimumPrice() {
+        Hotel lakeWood = new Hotel("LakeWood", 160);
+        Hotel bridgeWood = new Hotel("BridgeWood", 170);
+        Hotel ridgeWood = new Hotel("RidgeWood", 140);
+        hotel.addHotel(lakeWood);
+        hotel.addHotel(bridgeWood);
+        hotel.addHotel(ridgeWood);
+       Assertions.assertEquals(510,hotel.findCheapestHotel("12Sep2020","15Sep2020"));
     }
 }
 
