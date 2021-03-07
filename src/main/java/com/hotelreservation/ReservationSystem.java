f37a15e8782b24926ffba9f0c1fd5c1070af82ac
shravan-kaja <shravan.kaja@gmail.com>
2021-03-07 19:21:29 +0530

    Added: associated hotel with rating

--- src/main/java/com/hotelreservation/ReservationSystem.java
+++ src/main/java/com/hotelreservation/ReservationSystem.java
@@ -8,12 +8,12 @@ public class ReservationSystem {
         Hotel hotel = new Hotel();
         System.out.println("Welcome to hotel Reservation System");
         ReservationSystem reservationSystem = new ReservationSystem();
-        Hotel lakeWood = new Hotel("LakeWood", 110, 90);
-        Hotel bridgeWood = new Hotel("BridgeWood", 150, 50);
-        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150);
+        Hotel lakeWood = new Hotel("LakeWood", 110, 90, 3);
+        Hotel bridgeWood = new Hotel("BridgeWood", 150, 50, 4);
+        Hotel ridgeWood = new Hotel("RidgeWood", 220, 150, 5);
         hotel.addHotel(lakeWood);
         hotel.addHotel(bridgeWood);
         hotel.addHotel(ridgeWood);
-        hotel.findCheapestHotel("12Sep2020","27Sep2020");
+        hotel.findCheapestHotel("11Sep2020", "12 Sep2020");
     }
 }
